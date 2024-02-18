858c2a4e5fb3374c78804897c703f16092c9b355
Alwin Doss <alwindoss84@gmail.com>
2024-02-18 21:08:44 +0530

    init project
    Signed-off-by: Alwin Doss <alwindoss84@gmail.com>

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/alwindoss/lango
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "log/slog"
+
+func main() {
+	slog.Info("Welcome to Lango: a feature flag management system")
+}
